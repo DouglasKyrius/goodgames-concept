d0480378e646a2a6514ef1d3a370c3ff79f25dcb
DouglasKyrius <douglaskyrius@gmail.com>
2020-07-13 21:15:04 -0400

    Updates

---
+++ precache-manifest.764b6fc7e59e65461d77b6ea2ce64b57.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d9f4eecc577e8b6d2396988048abe2",
+    "url": "/goodgames-concept/index.html"
+  },
+  {
+    "revision": "31d3547f32b08cc02b71",
+    "url": "/goodgames-concept/static/css/main.0ab28f10.chunk.css"
+  },
+  {
+    "revision": "5781fd31b896746dfcb9",
+    "url": "/goodgames-concept/static/js/2.701b42e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goodgames-concept/static/js/2.701b42e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d3547f32b08cc02b71",
+    "url": "/goodgames-concept/static/js/main.3ce30303.chunk.js"
+  },
+  {
+    "revision": "97bd8add46346d9c3700",
+    "url": "/goodgames-concept/static/js/runtime-main.ffeae706.js"
+  },
+  {
+    "revision": "9ad4639cc61d858693b50171f32288cd",
+    "url": "/goodgames-concept/static/media/cyberpunk.9ad4639c.png"
+  },
+  {
+    "revision": "3979034bf75b024bb7bcb9b299936281",
+    "url": "/goodgames-concept/static/media/fire-icon.3979034b.png"
+  },
+  {
+    "revision": "00e85ab43813883a8e3dc6e5b16eaa25",
+    "url": "/goodgames-concept/static/media/fortnite.00e85ab4.png"
+  },
+  {
+    "revision": "3cef22366f80fbb516a6885c7acc29b2",
+    "url": "/goodgames-concept/static/media/halov2.3cef2236.png"
+  },
+  {
+    "revision": "9c3400dc82ef3f3e7efe8d97435754c8",
+    "url": "/goodgames-concept/static/media/horizon-wp2.9c3400dc.jpg"
+  },
+  {
+    "revision": "57259ddbff190a3cbdca67243c4696ff",
+    "url": "/goodgames-concept/static/media/horizon.57259ddb.jpg"
+  },
+  {
+    "revision": "8518e9d6e36a0e758482a4bb1769e8b2",
+    "url": "/goodgames-concept/static/media/kotaku.8518e9d6.webp"
+  },
+  {
+    "revision": "c58750c1f848130318e8de10acde179f",
+    "url": "/goodgames-concept/static/media/logo-icon.c58750c1.svg"
+  },
+  {
+    "revision": "e36f87e7012bbdc198c46c74b1a16000",
+    "url": "/goodgames-concept/static/media/ori.e36f87e7.png"
+  },
+  {
+    "revision": "dabf415eaa52ee3d305ad7b6190ca610",
+    "url": "/goodgames-concept/static/media/search-icon.dabf415e.png"
+  },
+  {
+    "revision": "c89f3a10a5731c85b68215972b9416f8",
+    "url": "/goodgames-concept/static/media/steam-logo.c89f3a10.png"
+  },
+  {
+    "revision": "a031ff66298e336452b385f4d429d27c",
+    "url": "/goodgames-concept/static/media/thelastofus.a031ff66.png"
+  },
+  {
+    "revision": "7ee3e219222020305f74872c2cbeb446",
+    "url": "/goodgames-concept/static/media/torchlight-wp.7ee3e219.jpg"
+  },
+  {
+    "revision": "7a144f7feac661f31152500e35919458",
+    "url": "/goodgames-concept/static/media/torchlight.7a144f7f.webp"
+  },
+  {
+    "revision": "6da635a448952d686aaddea055fa529b",
+    "url": "/goodgames-concept/static/media/usgamer-logo.6da635a4.jpg"
+  },
+  {
+    "revision": "a4d884f82cd8f62d43fd604db5c83c0f",
+    "url": "/goodgames-concept/static/media/valorant-wp.a4d884f8.jpg"
+  },
+  {
+    "revision": "5976c6949e22ca16302e3be9f5c8a076",
+    "url": "/goodgames-concept/static/media/valorant.5976c694.png"
+  },
+  {
+    "revision": "0663d045adbec6b5e9c126c40e3f5f45",
+    "url": "/goodgames-concept/static/media/wallpaperv2.0663d045.png"
+  }
+]);
